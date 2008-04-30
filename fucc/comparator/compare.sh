3e6dc45d9402a4a63c9ce4146c8fc9a69aaeaba6
afriho <afriho@c4720485-154c-0410-8f3b-6781356ca714>
2008-04-30 19:08:00 +0000

    added a license to all files
    git-svn-id: http://fucc.googlecode.com/svn/trunk@2 c4720485-154c-0410-8f3b-6781356ca714

--- fucc/comparator/compare.sh
+++ fucc/comparator/compare.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+#------------------------------------------------------------------------------#
+#    This file is part of fucc.                                                #
+#                                                                              #
+#    fucc is free software: you can redistribute it and/or modify              #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, version 3 of the License.                   #
+#                                                                              #
+#    fucc is distributed in the hope that it will be useful,                   #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with fucc.  If not, see <http://www.gnu.org/licenses/>.             # 
+#------------------------------------------------------------------------------#
 
 TESTCASE_DIRECTORY="$1"
 shift
--- fucc/gensen/grammar.py
+++ fucc/gensen/grammar.py
@@ -1,3 +1,19 @@
+
+#------------------------------------------------------------------------------#
+#    This file is part of fucc.                                                #
+#                                                                              #
+#    fucc is free software: you can redistribute it and/or modify              #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, version 3 of the License.                   #
+#                                                                              #
+#    fucc is distributed in the hope that it will be useful,                   #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    You should have received a copy of the GNU General Public License         #
+#    along with fucc.  If not, see <http://www.gnu.org/licenses/>.             # 
+#------------------------------------------------------------------------------#
 DEFAULT_OPTIONAL_NT_WEIGHT  = 0.5
 NONTERMINAL                 = 'NON-TERM'
 OPTIONAL_NT                 = 'OPT-NONT'
