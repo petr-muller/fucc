82f6b60491d105650732fc0df91659a974b23881
afriho <afriho@c4720485-154c-0410-8f3b-6781356ca714>
2008-05-27 23:58:41 +0000

    - mostly whitespace changes - some docstring addition - various pylint score chase
    git-svn-id: http://fucc.googlecode.com/svn/trunk@9 c4720485-154c-0410-8f3b-6781356ca714

--- fucc/spitter/grammar.py
+++ fucc/spitter/grammar.py
@@ -1,4 +1,5 @@
-
+"""This file implements a inner representation of grammar rules. All elements
+have their own class"""
 #------------------------------------------------------------------------------#
 #    This file is part of fucc.                                                #
 #                                                                              #
@@ -12,83 +13,94 @@
 #    GNU General Public License for more details.                              #
 #                                                                              #
 #    You should have received a copy of the GNU General Public License         #
-#    along with fucc.  If not, see <http://www.gnu.org/licenses/>.             # 
+#    along with fucc.  If not, see <http://www.gnu.org/licenses/>.             #
 #------------------------------------------------------------------------------#
+import sys
+
 DEFAULT_OPTIONAL_NT_WEIGHT  = 0.5
 NONTERMINAL                 = 'NON-TERM'
 OPTIONAL_NT                 = 'OPT-NONT'
 TERMINAL                    = 'TERMINAL'
-
+LOGGER                      = None
 def plugLogger(logger):
-  Element.logger = logger
+  """Plugs this module into an instance of a logger"""
+  global LOGGER
+  LOGGER = logger
 
-class Element:
-  def __init__(self):
-    Element.logger = None
-  def log(self, priority, message):
-    if Element.logger == None:
-      print >> sys.stderr, message
+def log(priority, message):
+  """Logs to a logger, honoring priority. If no logger, then log to stdout"""
+  if LOGGER is None:
+    print >> sys.stderr, message
+  else:
+    LOGGER.log(priority, message)
+
+class Symbol():
+  """Class representing one symbol"""
+  def __init__(self, symbol, symbol_type):
+    self.name = symbol
+    self.type = symbol_type
+  def __str__(self):
+    if self.type == NONTERMINAL:
+      return "<%s>" % self.name
     else:
-      Element.logger.log(priority, message)
-
-class Symbol(Element):
-    def __init__(self,symbol,type):
-        self.name = symbol
-        self.type = type
-    def __str__(self):
-        if self.type == NONTERMINAL:
-            return "<%s>" % self.name
-        else:
-            return self.name
+      return self.name
 
 class OptSymbol(Symbol):
-    def __init__(self,symbol,type,weight=DEFAULT_OPTIONAL_NT_WEIGHT):
-        Symbol.__init__(self,symbol,type)
-        self.weight = weight
-    def __str__(self):
-        return "<[%s]>{%s}" % (self.name, self.weight)
+  """Class representing one symbol with probability configuration"""
+  def __init__(self, symbol, symbol_type, weight=DEFAULT_OPTIONAL_NT_WEIGHT):
+    Symbol.__init__(self, symbol, symbol_type)
+    self.weight = weight
+  def __str__(self):
+    return "<[%s]>{%s}" % (self.name, self.weight)
 
-class Production(Element):
-    def __init__(self,symbol_list):
-        self.symbols = symbol_list
-    def __str__(self):
-        return "".join([str(sym) for sym in self.symbols])
+class Production():
+  """Class representing one possible production = sequence of symbols"""
+  def __init__(self, symbol_list):
+    self.symbols = symbol_list
+  def __str__(self):
+    return "".join([str(sym) for sym in self.symbols])
 
-class Rule(Element):
-    def __init__(self,symbol,productions,weights):
-        self.name = symbol
-        self.productions = []
-        for cnt in range(0,len(productions)):
-            try:
-                self.productions.append({"production" : productions[cnt], "weight" : weights[cnt]})
-            except IndexError:
-                self.productions.append({"production" : productions[cnt], "weight" : 1})
-    def __str__(self):
-        a = "Syntactic rule: <" + self.name + "> ::= "
-        ind = len("Syntactic rule: <" + self.name + "> ::")
+class Rule():
+  """Represents one rule = name and a list of possible productions"""
+  def __init__(self, symbol, productions, weights):
+    self.name = symbol
+    self.productions = []
+    for cnt in range(0, len(productions)):
+      try:
+        self.productions.append({ "production" : productions[cnt],
+                                  "weight" : weights[cnt]})
+      except IndexError:
+        self.productions.append({"production" : productions[cnt], "weight" : 1})
 
-        for i in self.productions:
+  def __str__(self):
+    msg = "Syntactic rule: <" + self.name + "> ::= "
+    ind = len("Syntactic rule: <" + self.name + "> ::")
 
-            a += str(i["production"]) + " (weight %s)" % (i["weight"]) + "\n" + ind*" " + '| '
-        a = a[:-(ind+3)] + ';;;'
-        return a
+    for i in self.productions:
+      msg += str(i["production"]) +\
+             " (weight %s)\n%s| " % (i["weight"], ind*" ")
+    msg = msg[:-(ind+3)] + ';;;'
+    return msg
 
-class Grammar(Element):
-    def __init__(self,rules):
-      self.symbols = {}
-      self.symbol_names = []
-      for rule in rules:
-        self.symbols[rule.name] = rule.productions
-        self.symbol_names.append(rule.name)
+class Grammar():
+  """Represents whole grammar = a dictionary of rules"""
+  def __init__(self, rules):
+    self.symbols = {}
+    self.symbol_names = []
+    for rule in rules:
+      self.symbols[rule.name] = rule.productions
+      self.symbol_names.append(rule.name)
 
-    def checkSanity(self):
-      for rule in self.symbol_names:
-        for production in self.symbols[rule]:
-          for symbol in production["production"].symbols:
-            if symbol.type == NONTERMINAL or symbol.type == OPTIONAL_NT:
-              if symbol.name not in self.symbol_names:
-                self.log(0,"Error: there's no expansion for nonterminal %s in rule for %s" % (symbol.name, rule))
+  def checkSanity(self):
+    """Check if all nonterminal in productions have a rule for generating"""
+    for rule in self.symbol_names:
+      for production in self.symbols[rule]:
+        for symbol in production["production"].symbols:
+          if symbol.type == NONTERMINAL or symbol.type == OPTIONAL_NT:
+            if symbol.name not in self.symbol_names:
+              msg = "Error: no expansion for nonterminal %s, rule for %s"
+              log(0, msg % (symbol.name, rule))
 
 
-    def __str__(self):
-        return "Grammar containing %s rules" % len(self.symbol_names)
+  def __str__(self):
+    return "Grammar containing %s rules" % len(self.symbol_names)
--- fucc/spitter/main.py
+++ fucc/spitter/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+"""This script is a driving script for Spitter, the random C sentences 
+generator."""
 #------------------------------------------------------------------------------#
 #    This file is part of fucc.                                                #
 #                                                                              #
@@ -12,7 +14,7 @@
 #    GNU General Public License for more details.                              #
 #                                                                              #
 #    You should have received a copy of the GNU General Public License         #
-#    along with fucc.  If not, see <http://www.gnu.org/licenses/>.             # 
+#    along with fucc.  If not, see <http://www.gnu.org/licenses/>.             #
 #------------------------------------------------------------------------------#
 
 from bnf_parser import yacc
@@ -24,42 +26,40 @@ import generator
 from optparse import OptionParser
 
 if __name__ == "__main__":
+  opt = OptionParser()
+  opt.add_option("-g", "--grammar", dest="grammar",
+                        help="use grammar defined in FILE", metavar="FILE")
+  opt.add_option("-v", "--verbose", default=0, dest="verbose",
+                        help="Verbosity level (0-2)", metavar="LEVEL")
+  opt.add_option("-b", "--blockdepth", default=5, dest="bd", metavar="DEPTH",
+                        help="Sets maximum of block repetition")
+  opt.add_option("-f", "--functions", default=1, dest="fc", metavar="MAX",
+                        help="Sets maximum of single function calls")
+  opt.add_option("-e", "--exptdepth", default=5, dest="ed", metavar="DEPTH",
+                        help="Sets average recursivity of expressions")
 
-    opt = OptionParser()
-    opt.add_option("-g", "--grammar", dest="grammar",
-                          help="use grammar defined in FILE", metavar="FILE")
-    opt.add_option("-v", "--verbose", default=0, dest="verbose",
-                          help="Verbosity level (0-2)", metavar="LEVEL")
-    opt.add_option("-b", "--blockdepth", default=5, dest="bd",
-                          help="Sets maximum of block repetition", metavar="DEPTH")
-    opt.add_option("-f", "--functions", default=1, dest="fc",
-                          help="Sets maximum of single function calls", metavar="MAX")
-    opt.add_option("-e", "--exptdepth", default=5, dest="ed",
-                          help="Sets average recursivity of expressions", metavar="DEPTH")
+  (options, args) = opt.parse_args()
+  if not options.grammar:
+    print >> sys.stderr, "Grammar file must be supplied"
+    sys.exit(1)
+  gram_file = open(options.grammar)
+  gram = gram_file.read()
+  gram_file.close()
 
-    (options, args) = opt.parse_args()
-    if not options.grammar:
-      print >> sys.stderr, "Grammar file must be supplied"
-      sys.exit(1)
+  my_logger = logger.Logger(sys.stdout, sys.stderr, int(options.verbose))
+  grammar.plugLogger(my_logger)
 
-    gram_file = open(options.grammar)
-    gram = gram_file.read()
-    gram_file.close()
+  my_grammar = yacc.parse(gram)
 
-    my_logger = logger.Logger(sys.stdout, sys.stderr, int(options.verbose))
-    grammar.plugLogger(my_logger)
+  direct = C_director.Director()
+  C_director.plugLogger(my_logger)
+  direct.loadGrammar(my_grammar)
+  direct.setMaxDepth(int(options.bd))
+  direct.setMaxFunction(int(options.fc))
+  direct.setExprDepth(int(options.ed))
 
-    my_grammar = yacc.parse(gram)
-
-    direct = C_director.Director()
-    C_director.plugLogger(my_logger)
-    direct.loadGrammar(my_grammar)
-    direct.setMaxDepth(int(options.bd))
-    direct.setMaxFunction(int(options.fc))
-    direct.setExprDepth(int(options.ed))
-
-    my_gen = generator.Generator()
-    generator.plugLogger(my_logger)
-    my_gen.plugDirector(direct)
-    print my_gen.generate('program')
+  my_gen = generator.Generator()
+  generator.plugLogger(my_logger)
+  my_gen.plugDirector(direct)
+  print my_gen.generate('program')
     
