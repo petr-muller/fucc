9dde609959b38d1ea625fe38106d701b6ed670e4
afriho <afriho@c4720485-154c-0410-8f3b-6781356ca714>
2008-06-12 12:52:19 +0000

    generator refactoring and fixes for pylint
    git-svn-id: http://fucc.googlecode.com/svn/trunk@11 c4720485-154c-0410-8f3b-6781356ca714

--- fucc/spitter/generator.py
+++ fucc/spitter/generator.py
@@ -1,4 +1,5 @@
-
+"""This module contains a generator, which uses EBNF as an input and produces
+random sentences as defined by that EBNF"""
 #------------------------------------------------------------------------------#
 #    This file is part of fucc.                                                #
 #                                                                              #
@@ -12,49 +13,76 @@
 #    GNU General Public License for more details.                              #
 #                                                                              #
 #    You should have received a copy of the GNU General Public License         #
-#    along with fucc.  If not, see <http://www.gnu.org/licenses/>.             # 
+#    along with fucc.  If not, see <http://www.gnu.org/licenses/>.             #
 #------------------------------------------------------------------------------#
 import random
 import grammar
 import exceptions
 import copy
+import sys
+
+LOGGER = None
 
 def plugLogger(logger):
-  "Just plugs the logger class here"
-  Generator.logger = logger
+  """Plugs this module into an instance of a logger"""
+  global LOGGER
+  LOGGER = logger
+
+def log(priority, message, indent=0):
+  """Logs to a logger, honoring priority. If no logger, then log to stdout"""
+  if LOGGER is None:
+    print >> sys.stderr, message
+  else:
+    LOGGER.log(priority, message, indent)
+
+def spit(message):
+  "Spits output. Uses Logger if possible"
+  if LOGGER is None:
+    print >> sys.stdout, message
+  else:
+    LOGGER.spit(message)
 
 class ProductionNotPossible(exceptions.Exception):
   "Only a exception class for reporting that production branch failed"
-  def __init__(self, message=""):
-    pass
+  def __init__(self, msg):
+    Exception.__init__(self)
+    self.msg = msg
+  def __str__(self):
+    return self.msg
 
 class Generator:
   "Generator class. Randomly generates sentences defined by a language grammar"
   def __init__(self):
     "Constructor does fairly nothing"
-    Generator.logger = None
     self.director = None
   
-  def plugDirector(self,director):
+  def plugDirector(self, director):
     "Plug for Director instance"
     self.director = director
 
-  def log(self, priority, message, indent=0):
-    "Logging method. Uses Logger if possible"
-    if Generator.logger is None:
-      print >> sys.stderr, message
-    else:
-      Generator.logger.log(priority, message,indent)
-  
-  def spit(self, message):
-    "Spits output. Uses Logger if possible"
-    if Generator.logger is None:
-      print >> sys.stdout, message
-    else:
-      Generator.logger.spit(message)
+  def _select_production(self, productions):
+    """Internal method which chooses one production from the list.
+       It honors weights"""
+    wtotal = sum([x['weight'] for x in productions])
+    roll = random.uniform(0, wtotal)
+    log(2, "Random choice: roll %s out of %s" % (roll, wtotal))
+    # it may happen all productions have weight of 0. Return None then.
+    if wtotal == 0:
+      return None
+    # select which production belongs to random number n
+    selected = None
+    for prod in productions:
+      selected = prod
+      if roll <= prod['weight']:
+        break
+      roll = roll-prod['weight']
 
-  def generate(self,symbol):
-    "Method for actually generate the sentence.Can backtrack if subbranch fails to generate"
+    return selected
+
+
+  def generate(self, symbol):
+    """Method for actually generate the sentence."""\
+    """"Can backtrack if subbranch fails to generate"""
     # obtain a list of weighted productions
     self.director.reportStart(symbol)
     productions = self.director.getSymbol(symbol)
@@ -63,62 +91,60 @@ class Generator:
     # while we are not successful, try to generate some production from the list
     while not success:
       # it may happen they are no productions. return None then
-      if len(productions) == 0:
-        self.log(2, "No valid productions left for symbol <%s>" % symbol)
+      try:
+        message = "No valid productions left for symbol <%s>" % symbol
+        if len(productions) == 0:
+          raise ProductionNotPossible(message)
+        success = True
+        selected = self._select_production(productions)
+        if selected is None:        
+          raise ProductionNotPossible(message)
+      except ProductionNotPossible, (exc_instance):
+        log(2, exc_instance.msg)
         self.director.reportFailure(symbol)
         return None
-
-      success = True
-      # select one production from the list and honor weights
-      wtotal = sum([x['weight'] for x in productions])
-      n = random.uniform(0, wtotal)
-      self.log(2, "Random choice: roll %s out of %s" % (n, wtotal))
-      # it may happen all productions have weight of 0. Return None then.
-      if wtotal == 0:
-        self.log(2, "No valid producitons left for symbol <%s>" % symbol)
-        self.director.reportFailure(symbol)
-        return None
-      # select which production belongs to random number n
-      for prod in productions:
-        if n <= prod['weight']:
-          break
-        n = n-prod['weight']
 
       try:
-        buffer = []
+        symbol_buffer = []
         # now try to generate all symbols from the production
-        for symb in prod['production'].symbols:
+        for symb in selected['production'].symbols:
           if symb.type == grammar.NONTERMINAL:
+            
             # nonterminal - try to generate, raise exception if fails
-            buffer.append(self.generate(symb.name))
-            if buffer[-1] == None:
+            symbol_buffer.append(self.generate(symb.name))
+            if symbol_buffer[-1] == None:
               raise ProductionNotPossible
           elif symb.type == grammar.OPTIONAL_NT and symb.weight <= 1.0:
-            if symb.weight >= random.uniform(0,1):
+            if symb.weight >= random.uniform(0, 1):
               # optional nonterminal - try to generate, omit if fails
-              buffer.append(self.generate(symb.name))
-              if buffer[-1] == None:
-                self.log(2, "Production of optional symbol <%s> not possible, omitting" % symb)
-                del buffer[-1]
+              symbol_buffer.append(self.generate(symb.name))
+              if symbol_buffer[-1] == None:
+                log(2, "Production of optional symbol <%s> not possible, \
+omitting" % symb)
+                del symbol_buffer[-1]
             else:
-              self.log(1, "Not generating optional symbol")
+              log(1, "Not generating optional symbol")
           elif symb.type not in (grammar.OPTIONAL_NT, grammar.NONTERMINAL):
             # terminals - print
-            buffer.append(symb.name)
-            self.log(2, "Writing nonterminal")
+            symbol_buffer.append(symb.name)
+            log(2, "Writing nonterminal")
           else:
             # if something weird happens, then report it
-            self.log(1, "Warning: there is some possible error in rules for symbol <%s>" % symbol)
+            log(1, "Warning: there is some possible error in rules \
+for symbol <%s>" % symbol)
 
-      # if some branch failed to generate, try to select another production and generate it
+      # if some branch failed to generate, try to select another production
+      # and try to generate it instead
       except ProductionNotPossible:
-        a = productions.index(prod) 
-        self.log(2, "Production at index %s for symbol <%s> not possible, omitting and backtracking" % (a,symbol))
+        ind = productions.index(selected) 
+        log(2, "Production at index %s for symbol <%s> not possible, \
+omitting and backtracking" % (ind,symbol))
         success = False
 
-        # we have to deepcopy production, as we dont want to erase the production from the master grammar
+        # we have to deepcopy production
+        # we dont want to erase the production from the master grammar
         productions = copy.deepcopy(productions)
-        del productions[a]
+        del productions[ind]
 
-    tmp = "".join(buffer)
-    return self.director.reportFinish(symbol,tmp)
+    tmp = "".join(symbol_buffer)
+    return self.director.reportFinish(symbol, tmp)
